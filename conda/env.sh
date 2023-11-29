c0a9102317d8cb59bf6bad6a1c540a65212f0f24
Tonmoy Das <dtonmoy0@gmail.com>
2023-11-29 09:19:15 +0100

    add new conda commands

--- conda/env.sh
+++ conda/env.sh
@@ -1,14 +1,19 @@
-#-----
+# -----
 # Create a new conda environment inside a directory with prefix
-#-----
+# -----
 conda create --prefix ./env pandas numpy matplotlib scikit-learn
 
-#-----
+# -----
 # Create a new conda environment with name
-#-----
+# -----
 conda create --name env_name pandas numpy matplotlib scikit-learn
 
-#-----
+# -----
 # Remove a conda environment
-#-----
+# -----
 conda env remove -–prefix "conda_env"
+
+# -----
+# clone an existing conda repository using prefix
+# -----
+mamba create --prefix new_env --clone path/to/file/env1
