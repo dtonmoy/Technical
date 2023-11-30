835d37c6a9a35a38cf4ef87dcc0f28374d7bf768
Tonmoy Das <dtonmoy0@gmail.com>
2023-11-30 11:35:51 +0100

    update kernelspec commands

--- conda/kernelspec.sh
+++ conda/kernelspec.sh
@@ -1,4 +1,14 @@
-#-----
-# Install a new kernelspec
-#-----
+# -----
+# Install a new kernelspec for python
+# -----
 python -m ipykernel install --user --name kernel_name
+
+# -----
+# Remove a kernelspec
+# -----
+jupyter kernelspec remove kernel_name
+
+# -----
+# Add a new kernelspec for R (inside R)
+# -----
+IRkernel::installspec(name = 'kernel_name', displayname = 'kernel_name')
