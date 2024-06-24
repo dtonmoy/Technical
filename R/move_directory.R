e4ae93108bc69099c0200852cafa7ddc5767c5d8
Tonmoy Das <dtonmoy0@gmail.com>
2024-06-24 11:21:19 +0200

    Update move directory script

--- R/move_directory.R
+++ R/move_directory.R
@@ -1,31 +1,31 @@
-##########
+# ##########
 # Load libraries
 library(ff)
 
-##########
+# ##########
 # Set parameters
 from <- "/Directory/containing/the/folders/that/are/to/be/moved"
 to <- "/Directory/where/the/folders/will/be/moved"
 
-##########
+# ##########
 # Get the directory lists
 dirLists <- list.dirs(from, recursive = FALSE)
 
 for (i in dirLists) {
     
-    #-----
+    # -----
     # get the new directory paths
-    #-----
+    # -----
     newDir <- file.path(to, basename(i))
     
-    #-----
+    # -----
     # create new directory to move files
-    #-----
+    # -----
     dir.create(newDir, recursive = TRUE, showWarnings = TRUE)
     
-    #-----
+    # -----
     # move the files
-    #-----
+    # -----
     file.move(i, newDir)
     
 }
